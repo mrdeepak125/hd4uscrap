0a014c4b14012716f9e3afa8ecf3d62121c12d2b
Deepak Goswami <asus0gl8@outlook.com>
2025-07-20 08:23:44 +0530

    Update stream_scraper.js

--- stream_scraper.js
+++ stream_scraper.js
@@ -4,7 +4,7 @@ const cheerio = require('cheerio');
 
 class StreamScraper {
     constructor() {
-        this.baseUrl = 'https://hdhub4u.gs';
+        this.baseUrl = 'https://hdhub4u.family';
         this.headers = {
             'Cookie': 'xla=s4t',
             'Referer': 'https://google.com',
